f859322b8c91fdf5f8e40c5a1e9108f81a610be1
hari07 <hariodukkathil@gmail.com>
2022-02-08 23:33:26 +0530

    first react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css"
+import {useState} from 'react'
 function App() {
+  const [count,setcount] = useState(0)
+  const addCount = () =>{
+      setcount(count+1)
+      console.log(setcount)
+      
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div> 
+   <h1 className="hello">counter: {count}</h1>
+   <button onClick={addCount}>add</button>
+   
+   </div>
   );
 }
 
---
+++ src/components/header.js
@@ -0,0 +1,6 @@
+import React from "react";
+function Header(props){
+return(<h1>This is the {props.data} from header.js</h1>)
+}
+
+export default Header;
